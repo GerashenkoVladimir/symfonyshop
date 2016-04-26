543bed7f67ddaba20cd1a13d72eb95426496cd11
Vladimir Gershenko <gera8469@gmail.com>
2016-04-26 22:11:36 +0300

    faeture(AdminBundle): add categories form(in process)

--- src/AdminBundle/Controller/AdminController.php
+++ src/AdminBundle/Controller/AdminController.php
@@ -2,22 +2,48 @@
 
 namespace AdminBundle\Controller;
 
+use ShopBundle\Entity\Categories;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends Controller
 {
     public function indexAction()
     {
+
+
         //убрать 
         $repository = $this->getDoctrine()->getRepository('ShopBundle:Categories');
         $categories = $repository->findAll();
         return $this->render('AdminBundle::index.html.twig',array( 'categories' => $categories));
     }
 
-    public function addCategoryAction()
+    public function addCategoryAction(Request $request)
     {
-        
+        $category = new Categories();
+        $form = $this->createFormBuilder($category, array(
+            'action' => $this->generateUrl('admin_add_category')
+        ))
+            ->add('name', TextType::class)
+//            ->add('image', FileType::class)
+            ->add('save',SubmitType::class, array('label' => 'Save'))
+            ->getForm();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+            return $this->redirectToRoute('admin_add_category');
+        }
+
+        //убрать
+        $repository = $this->getDoctrine()->getRepository('ShopBundle:Categories');
+        $categories = $repository->findAll();
+        return $this->render('AdminBundle:actions:addCategory.html.twig', array('form' => $form->createView(), 'categories' => $categories));
     }
 
     public function loginAction(Request $request)
